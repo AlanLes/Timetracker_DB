48ebd567caa8d3c14cc8432fb06c3bd4f310b20e
Alan Leszczuk <alan.leszczuk@altimi.com>
2016-04-07 14:22:52 +0200

    in the middle of creating models

--- timetrackersite/timetrackerapp/models.py
+++ timetrackersite/timetrackerapp/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# model kalendarz, pk - id generuje sie automatycznie, wiec data nie jest pk
+class Kalendarz(models.Model):
+    data = models.DateField(default=timezone.now)
+    czy_pracujacy = models.BooleanField
+    uwagi = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.data
+
+
+class CzasPracy(models.Model):
+    calendar = models.ForeignKey(Kalendarz)
+    czas_przyjscia = models.TimeField
