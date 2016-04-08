a648443876f2426b455975257b26dc63698e8709
AlanLes <a.leszczuk94@gmail.com>
2016-04-08 15:33:45 +0200

    null=True (for the datebase)

--- timetrackersite/timetrackerapp/models.py
+++ timetrackersite/timetrackerapp/models.py
@@ -6,65 +6,57 @@ from django.utils import timezone
 
 # model kalendarz, pk - id generuje sie automatycznie, wiec data nie jest pk
 class Kalendarz(models.Model):
-    data = models.DateField(default=timezone.now)
-    czy_pracujacy = models.BooleanField(initial=True)
-    uwagi = models.CharField(max_length=200)
+    data = models.DateField(default=timezone.now, null=False)
+    czy_pracujacy = models.BooleanField(default=True, null=False)
+    uwagi = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return 'Data: ' + self.data
 
 
 class Przerwa(models.Model):
-    start_przerwy = models.TimeField()
-    koniec_przerwy = models.TimeField()
-    calkowity_czas_przerwy = models.DurationField()
+    start_przerwy = models.TimeField(null=False)
+    koniec_przerwy = models.TimeField(null=True)
+    calkowity_czas_przerwy = models.DurationField(null=True)
 
     def __str__(self):
         return 'Laczny czas przerwy' + self.calkowity_czas_przerwy
 
 
 class CzasPracy(models.Model):
-    kalendarz_id = models.ForeignKey(Kalendarz)
-    czas_przyjscia = models.TimeField()
-    czas_wyjscia = models.TimeField()
-    przerwa_id = models.ForeignKey(Przerwa)
-    calkowity_czas_pracy = models.DurationField()
+    kalendarz_id = models.ForeignKey(Kalendarz, null=False)
+    czas_przyjscia = models.TimeField(null=False)
+    czas_wyjscia = models.TimeField(null=True)
+    przerwa_id = models.ForeignKey(Przerwa, null=True)
+    calkowity_czas_pracy = models.DurationField(null=True)
 
     def __str__(self):
         return 'Laczny czas pracy: ' + self.calkowity_czas_pracy
 
 
 class Pracownik(models.Model):
-    przelozony_id = models.ForeignKey('self')
-    imie = models.CharField(max_length=20)
-    nazwisko = models.CharField(max_length=30)
-    stanowisko = models.CharField(max_length=20)
-    email = models.EmailField()
-    haslo = models.CharField(max_length=30)
+    przelozony_id = models.ForeignKey('self', null=True)
+    imie = models.CharField(max_length=20, null=False)
+    nazwisko = models.CharField(max_length=30, null=False)
+    stanowisko = models.CharField(max_length=20, null=False)
+    email = models.EmailField(null=False)
+    haslo = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return 'Pracownik ' + self.imie + ' ' + self.nazwisko + ' zajmujacy stanowisko ' + self.stanowisko
 
-    def validateEmail(email):
-        from django.core.validators import validate_email
-        from django.core.exceptions import ValidationError
-        try:
-            validate_email(email)
-            return True
-        except ValidationError:
-            return False
     # to do: write some imie and nazwisko validators.
 
 
 class Urlop(models.Model):
-    pracownik_id = models.ForeignKey(Pracownik)
-    typ_urlopu = models.CharField(max_length=30)
+    pracownik_id = models.ForeignKey(Pracownik, null=False)
+    typ_urlopu = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return 'Urlop typu ' + self.typ_urlopu+ ' pracownika o id: ' + self.pracownik_id
 
 
 class DzienPracownika(models.Model):
-    czas_pracy_id = models.ForeignKey(CzasPracy)
-    urlop_id = models.ForeignKey(Urlop)
-    pracownik_id = models.ForeignKey(Pracownik)
+    czas_pracy_id = models.ForeignKey(CzasPracy, null=False)
+    urlop_id = models.ForeignKey(Urlop, null=False)
+    pracownik_id = models.ForeignKey(Pracownik, null=False)
